1ab5288d770f11b053e172440807c7faf23361bd
BIT <BIT@DESKTOP-F3NN5PP>
2019-03-20 09:30:24 +0900

    Java Config

---
+++ src/main/java/com/douzone/springcontainer/videosystem/DVDPlayer.java
@@ -0,0 +1,21 @@
+package com.douzone.springcontainer.videosystem;
+
+public class DVDPlayer {
+	private DigitalVideoDisc dvd;
+	
+	public DVDPlayer(DigitalVideoDisc dvd) {
+		this.dvd = dvd;
+	}
+	
+	public DVDPlayer() {
+		
+	}
+	
+	public void play() {
+		dvd.play();
+	}
+	
+	public void setDigitalVideoDisc(DigitalVideoDisc dvd) {
+		this.dvd = dvd;
+	}
+}
